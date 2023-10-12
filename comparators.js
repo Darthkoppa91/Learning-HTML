fd052814e03fb764b6756cce3ebd6f975b70b11d
Darth_Koppa91 aka Aleks Koppa <94325606+Darthkoppa91@users.noreply.github.com>
2023-10-12 21:28:31 +0200

    Js. test operators

---
+++ comparators.js
@@ -0,0 +1,8 @@
+console.log (true+true);
+
+console.log(0 ===null);
+
+console.log(undefined == null);
+
+
+
---
+++ exercises_operators_index.js
@@ -0,0 +1,113 @@
+//var kAmIlA ;
+//var 3some ;
+
+var mood="stressfull";
+
+console.log(mood)
+console.log(mood.length)
+console.log(mood[5]);
+
+
+var a = 3
+var b= 7
+var c= 31
+
+console.log(a+b+c);
+console.log(b-a-c);
+
+console.log(c*(a-b));
+
+console.log(b/(a+c));
+console.log(b-(a/c));
+
+var banana= 1.2
+var gramage="gr"
+console.log(banana*1000)
+console.log(banana*1000 +gramage)
+
+var min= 60;
+var Anna = min*5;
+console.log(Anna);
+var Tom= min * 30;
+console.log(Tom);
+
+var memmory=4;
+var MB=1000;
+var GB="MB";
+console.log(memmory*MB+GB);
+
+var article= 5;
+var size_article=98;
+var size= "kb";
+console.log(size_article/article+size);
+
+var a = a*2;
+/*2*= a;
+b= b+3
+b+=3
+c= c/5
+c/=5;
+d= d-7;
+d-=7;
+*/
+var ime ="Aleks";
+var age= 32;
+var haveCat= false;
+
+console.log(typeof ime, typeof age, typeof haveCat);
+
+
+console.log(typeof "number",typeof true,typeof null,typeof "false",typeof 56);
+var p = true;
+var q = false;
+
+console.log(p && q );
+
+var age = 55;
+//var is_age_neg = > 0;
+//var greater = > 120;
+console.log(age<0);
+console.log(age>120);
+
+var limit_min= 60;
+var limit_max= 120;
+var speed= 88;
+
+
+console.log(speed>=limit_min)
+console.log(speed>=limit_max)
+
+if (2<3){
+    console.log("true");
+    
+}
+else if(2>4) {
+    console.log();
+
+}   
+else {
+
+    console.log("false");
+}
+
+var x= 18;
+// ukoliko zelimo i onda dodamo logicki operator //
+if(x%3 ===0 && x % 5 ===0){
+    console.log("deljiv sa 3 i sa 5")
+
+}
+else if (x%5 ===0){
+    console.log("deljiv sa 5")
+}
+else if (x%3 ===0){
+    console.log("deljiv sa 3")
+}
+
+else{
+    console.log("nedeljiv sa 3")
+
+}
+
+
+
+
